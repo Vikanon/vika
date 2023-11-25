a2f581c9755e77d26e117bd5a45dd304e6e83c96
Vikanon <152006540+Vikanon@users.noreply.github.com>
2023-11-25 17:08:07 +0500

    Add files via upload

---
+++ vika.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vika : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ну привет :)
+        print("hello woooorld");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
